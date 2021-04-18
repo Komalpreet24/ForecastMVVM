3383b4c968900af5dd9cce44db155b6574020226
komalpreet24 <kaur242k@gmail.com>
2021-04-18 17:45:27 +0530

    Location Change options in Settings

--- app/src/main/java/com/example/forecastmvvm/ForecastApplication.kt
+++ app/src/main/java/com/example/forecastmvvm/ForecastApplication.kt
@@ -1,15 +1,17 @@
 package com.example.forecastmvvm
 
+import LocationProviderImpl
 import android.app.Application
+import android.content.Context
 import com.example.forecastmvvm.data.db.ForcastDatabase
 import com.example.forecastmvvm.data.network.*
 import com.example.forecastmvvm.data.provider.LocationProvider
-import com.example.forecastmvvm.data.provider.LocationProviderImpl
 import com.example.forecastmvvm.data.provider.UnitProvider
 import com.example.forecastmvvm.data.provider.UnitProviderImpl
 import com.example.forecastmvvm.data.repository.ForecastRepository
 import com.example.forecastmvvm.data.repository.ForecastRepositoryImpl
 import com.example.forecastmvvm.ui.weather.current.CurrentWeatherViewModelFactory
+import com.google.android.gms.location.LocationServices
 import com.jakewharton.threetenabp.AndroidThreeTen
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
@@ -34,8 +36,9 @@ class ForecastApplication : Application(), KodeinAware{
         }
         bind<ConnectivityInterceptor>() with singleton { ConnectivityInterceptorImpl(instance()) }
         bind() from singleton { ApiService(instance()) }
-        bind<LocationProvider>() with singleton { LocationProviderImpl() }
+        bind<LocationProvider>() with singleton { LocationProviderImpl(instance(), instance()) }
         bind<WeatherNetworkDataSource>() with singleton { WeatherNetworkDataSourceImpl(instance()) }
+        bind() from provider { LocationServices.getFusedLocationProviderClient(instance<Context>()) }
         bind<ForecastRepository>() with singleton { ForecastRepositoryImpl(instance(), instance(), instance(), instance() )}
         bind<UnitProvider>() with singleton {  UnitProviderImpl(instance()) }
         bind() from provider { CurrentWeatherViewModelFactory(instance(), instance()) }
